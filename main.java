3cafdd9fe62f8be19b8db6b3512828077b8e1b30
Alexander Andreev <alexandreew@mail.ru>
2021-07-13 22:04:04 +0700

    adedd new main

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
  class Main {
     public static void main(String[] args) {
-        int price = 2_000_000_000;
-        int count = 500_000_000;
-        int total = price + count;
+        int balance = 2_000_000_000;
+        int coming = 500_000_000;
+        int total = balance + coming;
         System.out.println(total);
     }
 }
